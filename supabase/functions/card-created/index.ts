14022dfd231394e256c387ce0766082a729132dc
Bill Nizeyimana <bni@escaux.com>
2024-02-13 22:07:29 +0100

    add webhook

--- supabase/functions/card-created/index.ts
+++ supabase/functions/card-created/index.ts
@@ -2,7 +2,6 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-console.log("Hello from Functions!")
 import Stripe from 'stripe'
 
 
@@ -14,6 +13,7 @@ const stripe = new Stripe(Deno.env.get('STRIPE_API_KEY') as string, {
 })
 // This is needed in order to use the Web Crypto API in Deno.
 const cryptoProvider = Stripe.createSubtleCryptoProvider()
+
 
 Deno.serve(async (req) => {
   const { name } = await req.json()
@@ -32,7 +32,7 @@ Deno.serve(async (req) => {
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/card-holder-update' \
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/card-created' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --data '{"name":"Functions"}'
